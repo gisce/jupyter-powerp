f7e664b2fed81546072632e7f622ddacb8636648
Axel Simón <asimon@gisce.net>
2016-11-29 15:18:54 +0100

    Improve requirements check

--- startup.py
+++ startup.py
@@ -1,13 +1,17 @@
 import sys
 import logging
 import os
+logger = logging.getLogger('jupyter-powererp.OpenERPService')
+logger.setLevel(logging.DEBUG)
+if 'OPENERP_ROOT_PATH' not in os.environ:
+    logger.error('OPENERP_ROOT_PATH variable not set')
+    print('OPENERP_ROOT_PATH variable not set')
 sys.path.append(os.environ['OPENERP_ROOT_PATH'])
+
 from backend import OpenERPService
-from backend import Transaction
 from backend import PoolWrapper
 
 
-logger = logging.getLogger('jupyter-powererp.OpenERPService')
 logging.basicConfig(level=logging.INFO)
 
 logger.info('Using native OOOP')
@@ -15,7 +19,3 @@ service = OpenERPService()
 service.db_name = "dbname"
 uid = service.login("user", "password")
 O = PoolWrapper(service.pool, service.db_name, uid)
-
-polissa_model = O.model('giscedata.polissa')
-pol = polissa_model.browse(2)
-print("{}".format(pol.name))
