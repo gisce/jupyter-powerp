3477aeedffe08513794ad8d588fb653f91d79d87
Axel Simón <asimon@gisce.net>
2016-11-29 16:02:03 +0100

    Add configuration variables

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Required environment variables:
 - OPENERP_ROOT_PATH
+- OPENERP_DB_NAME
+- OPENERP_USER
+- OPENERP_PASSWORD
 
 How to run:
-OPENERP_ROOT_PATH="/home/user"  PYTHONSTARTUP="./startup.py" jupyter notebook
+OPENERP_DB_NAME="db" OPENERP_USER="user" OPENERP_PASSWORD="password" OPENERP_ROOT_PATH="/home/user"  PYTHONSTARTUP="./startup.py" jupyter notebook
--- startup.py
+++ startup.py
@@ -1,12 +1,21 @@
 import sys
 import logging
 import os
+import osconf
+
+required_vars = ['user', 'password']
+config = osconf.config_from_environment('OPENERP', required_vars)
+
 logger = logging.getLogger('jupyter-powererp.OpenERPService')
 logger.setLevel(logging.DEBUG)
 if 'OPENERP_ROOT_PATH' not in os.environ:
     logger.error('OPENERP_ROOT_PATH variable not set')
     print('OPENERP_ROOT_PATH variable not set')
 sys.path.append(os.environ['OPENERP_ROOT_PATH'])
+
+if 'OPENERP_DB_NAME' not in os.environ:
+    logger.error('OPENERP_DB_NAME variable not set')
+    print('OPENERP_DB_NAME variable not set')
 
 from backend import OpenERPService
 from backend import PoolWrapper
@@ -16,6 +25,6 @@ logging.basicConfig(level=logging.INFO)
 
 logger.info('Using native OOOP')
 service = OpenERPService()
-service.db_name = "dbname"
-uid = service.login("user", "password")
+service.db_name = os.environ['OPENERP_DB_NAME']
+uid = service.login(config['user'], config['password'])
 O = PoolWrapper(service.pool, service.db_name, uid)
