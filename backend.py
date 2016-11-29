314375225e91fe3af3bec6576c292b558eab6e7d
Axel Simón <asimon@gisce.net>
2016-11-29 16:31:49 +0100

    Improves the backend documentation

--- backend.py
+++ backend.py
@@ -6,12 +6,28 @@ from osconf import config_from_environment
 
 
 def camel2dot(name):
+    """
+    Converts a model name from _ to .
+
+    :param name: Module name
+    :return: Module name converted
+    """
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1.\2', name)
     return re.sub('([a-z])([A-Z0-9])', r'\1.\2', s1).lower()
 
 
 class OpenERPService(object):
+    """
+    OpenERP Service
+    """
+
     def __init__(self, **kwargs):
+        """
+        Creates a new OpenERP service.
+
+        :param kwargs: keyword arguments passed to the config
+        """
+
         import sys
         sys.argv = [sys.argv[0]]
         config = config_from_environment('OPENERP', [], **kwargs)
@@ -43,15 +59,33 @@ class OpenERPService(object):
 
     @property
     def db_name(self):
+        """
+        Database name
+        :return: Name of the database as str
+        """
+
         return self.config['db_name']
 
     @db_name.setter
     def db_name(self, value):
+        """
+        Sets ths name of the database
+        :param value: Name of the database
+        :return: None
+        """
+
         self.config['db_name'] = value
         self.db, self.pool = self.pooler.get_db_and_pool(self.db_name)
         # TODO: Patch ir.cron
 
     def login(self, user, password):
+        """
+        Logs into the database
+
+        :param user: OpenERP User
+        :param password: OpenERP Password
+        :return: OpenERP Connection
+        """
         if not self.db_name:
             raise Exception('Database not ready')
         import netsvc
@@ -60,17 +94,36 @@ class OpenERPService(object):
         return res
 
     def shutdown(self):
+        """
+        Closes all the OpenERP Service database connections
+
+        :return: None
+        """
+
         if self.db_name:
             import sql_db
             sql_db.close_db(self.db_name)
 
     def __del__(self):
+        """
+        Class destructor
+
+        :return: None
+        """
+
         self.shutdown()
 
 
 class Transaction(object):
+    """
+    OpenERP Transaction
+    """
 
     def __init__(self):
+        """
+        Creates a Transaction
+        """
+
         self.database = None
         self.service = None
         self.pool = None
@@ -79,6 +132,15 @@ class Transaction(object):
         self.context = None
 
     def start(self, database_name, user=1, context=None):
+        """
+        Starts a Transaction
+
+        :param database_name: Name of the database as a str
+        :param user: User for the transaction
+        :param context: Context for the Transaction
+        :return: Transaction context
+        """
+
         self._assert_stopped()
         self.service = OpenERPService(db_name=database_name)
         self.pool = self.service.pool
@@ -88,7 +150,12 @@ class Transaction(object):
         return self
 
     def stop(self):
-        'End the transaction'
+        """
+        End the transaction
+
+        :return: None
+        """
+
         self.cursor.close()
         self.service = None
         self.cursor = None
@@ -98,20 +165,43 @@ class Transaction(object):
         self.pool = None
 
     def get_context(self):
-        'Loads the context of the current user'
+        """
+        Loads the context of the current user
+
+        :return: Context object
+        """
+
         assert self.user is not None
 
         user_obj = self.pool.get('res.users')
         return user_obj.context_get(self.cursor, self.user)
 
     def __enter__(self):
+        """
+        Enter to the context
+
+        :return: Context
+        """
         return self
 
     def __exit__(self, type, value, traceback):
+        """
+        Exits the context stoping the transaction
+
+        :param type:
+        :param value:
+        :param traceback:
+        :return: None
+        """
         self.stop()
 
     def _assert_stopped(self):
-        'Assert that there is no active transaction'
+        """
+        Assert that there is no active transaction
+
+        :return: None
+        """
+
         assert self.service is None
         assert self.database is None
         assert self.cursor is None
@@ -121,7 +211,18 @@ class Transaction(object):
 
 
 class PoolWrapper(object):
+    """
+    Pool wrapper class
+    """
     def __init__(self, pool, dbname, uid):
+        """
+        Pool Wrapper constructor
+
+        :param pool:
+        :param dbname: Database name
+        :param uid: User identifier
+        """
+
         self.pool = pool
         self.dbname = dbname
         self.uid = uid
@@ -129,6 +230,13 @@ class PoolWrapper(object):
         self.fork = False
 
     def __getattr__(self, name):
+        """
+        Gets the value of a property
+
+        :param name: property name
+        :return: Returns the value of a property
+        """
+
         if not self.fork and (os.getpid() != self.ppid):
             import sql_db
             sql_db.close_db(self.dbname)
@@ -141,15 +249,37 @@ class PoolWrapper(object):
         return self.model(name)
 
     def model(self, name):
+        """
+        Gets the model
+
+        :param name: Name of the model
+        :return: Model object
+        """
+
         return ModelWrapper(self.pool.get(name), self.dbname, self.uid)
 
     @property
     def models(self):
+        """
+        Gets the list of OpenERP models
+        :return: List of OpenERP models
+        """
         return self.pool.obj_pool.keys()
 
 
 class ModelWrapper(object):
+    """
+    Model wrapper class
+    """
     def __init__(self, model, dbname, uid):
+        """
+        Creates a model wrapper
+
+        :param model: Name of the model
+        :param dbname: Database Name
+        :param uid: User id
+        """
+
         self.model = model
         self.dbname = dbname
         self.uid = uid
@@ -168,6 +298,10 @@ class ModelWrapper(object):
             return base
 
     def __del__(self):
-        print("Cursor!: {}".format(self.txn.cursor))
+        """
+        Model destructor
+
+        :return: None
+        """
         if self.txn.cursor:
             self.txn.stop()
